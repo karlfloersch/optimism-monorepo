31a64f0f4873f4ecdb1278ea1d919ba69115f360
Karl Floersch <karl@karlfloersch.com>
2020-04-19 19:13:31 -0400

    Attempt more config changes

--- docker/openethereum/entrypoint.sh
+++ docker/openethereum/entrypoint.sh
@@ -4,4 +4,5 @@ PRIVATE_KEY_PATH="${VOLUME_PATH}${PRIVATE_KEY_PATH_SUFFIX}"
 echo "$PRIVATE_KEY" | sed 's/^0x//' > $PRIVATE_KEY_PATH
 
 # ./parity --base-path $VOLUME_PATH --config="/home/parity/entrypoint.sh"  --jsonrpc-interface all --jsonrpc-hosts="all" --jsonrpc-port=$PORT --gasprice 0 -l error
-./parity --config="/home/parity/config.toml" --jsonrpc-port=9545 --min-gas-price 0 -l trace
+./parity --config="/home/parity/config.toml" --chain="/home/parity/instant_seal.json" --jsonrpc-port=9545 --min-gas-price 0 -l trace --tx-queue-no-early-reject --tx-queue-mem-limit=50 --tx-gas-limit=9000000000 --tx-time-limit=10000000
+# ./parity -h
