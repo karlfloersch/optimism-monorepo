b9470b15bb74759246c7ccdc2bd48a121d4db606
Karl Floersch <karl@karlfloersch.com>
2020-04-19 17:04:34 -0400

    Continue messing around with openeth config

--- docker/openethereum/entrypoint.sh
+++ docker/openethereum/entrypoint.sh
@@ -4,4 +4,4 @@ PRIVATE_KEY_PATH="${VOLUME_PATH}${PRIVATE_KEY_PATH_SUFFIX}"
 echo "$PRIVATE_KEY" | sed 's/^0x//' > $PRIVATE_KEY_PATH
 
 # ./parity --base-path $VOLUME_PATH --config="/home/parity/entrypoint.sh"  --jsonrpc-interface all --jsonrpc-hosts="all" --jsonrpc-port=$PORT --gasprice 0 -l error
-./parity --config="/home/parity/config.toml" --jsonrpc-port=9545 --gasprice 0 -l trace
+./parity --config="/home/parity/config.toml" --jsonrpc-port=9545 --min-gas-price 0 -l trace
